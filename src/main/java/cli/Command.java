fa284cd863daee6a413197ec799eb15940210d6d
Alysson Bruno <green.plan8941@pudim.org>
2024-11-03 17:25:27 -0300

    A simple PIPE implementation

--- src/main/java/cli/Command.java
+++ src/main/java/cli/Command.java
@@ -1,5 +1,8 @@
 package cli;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import repl.CommandREPL;
 
 /**
@@ -9,14 +12,33 @@ import repl.CommandREPL;
  */
 public class Command {
 
+    private static void readFromPipe() {
+        try {
+            if (System.in.available() > 0) {
+                BufferedReader pipeReader = new BufferedReader(
+                    new InputStreamReader(System.in)
+                );
+                String line;
+                while ((line = pipeReader.readLine()) != null) {
+                    System.out.printf("add %s\n", line);
+                }
+            } else {}
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         if (args.length > 0) {
-            for (String arg : args) {
-                System.out.print(arg + ":");
+            switch (args[0]) {
+                case "--pipe":
+                    readFromPipe();
+                    break;
             }
             System.out.println("It's Work!");
             return;
+        } else {
+            CommandREPL.terminal_repl();
         }
-        CommandREPL.terminal_repl();
     }
 }
