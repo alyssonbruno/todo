0edccc9d904beb55b588a4e6ac4b0f9c13862ee0
Alysson Bruno <green.plan8941@pudim.org>
2024-11-04 07:06:37 -0300

    Now can read from file. File and PIPE have two format: text and code.

--- src/main/java/cli/CommandCLI.java
+++ src/main/java/cli/CommandCLI.java
@@ -1,8 +1,11 @@
 package cli;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
 import java.util.concurrent.Callable;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
@@ -29,17 +32,55 @@ public class CommandCLI implements Callable<Integer> {
     )
     Boolean isPipe = false;
 
+    @Option(
+        names = { "-f", "--file" },
+        description = "a file with tasks to add"
+    )
+    File file = null;
+
+    @Option(
+        names = { "--file-format" },
+        description = "Should be text or code. Text (default) create a task for all full line, code create only lines with 'todo:'"
+    )
+    String format = "text";
+
+    /*  todo: others options: add, list, start, complete, delete
+        todo: how to make test
+        todo: implements save do file database
+    */
+    /** Receive a stream of information from PIPE then add one taks
+     *  for each line from it. If --file-format is text (default)
+     *  all line will be include, if is code only line with "todo:"
+     *  will be added.
+     * @author Alysson
+     * @version 2024-11
+     */
     private void readFromPipe() {
         try {
-            if (System.in.available() > 0) {
-                BufferedReader pipeReader = new BufferedReader(
+            BufferedReader pipeReader;
+            String line;
+
+            if (file != null) {
+                pipeReader = Files.newBufferedReader(file.toPath());
+            } else {
+                pipeReader = new BufferedReader(
                     new InputStreamReader(System.in)
                 );
-                String line;
-                while ((line = pipeReader.readLine()) != null) {
+            }
+
+            while ((line = pipeReader.readLine()) != null) {
+                if (format.equals("code")) {
+                    if (line.contains("todo:")) {
+                        int index = line.indexOf("todo:");
+                        System.out.printf(
+                            "add %s\n",
+                            line.substring(index + 5)
+                        );
+                    }
+                } else {
                     System.out.printf("add %s\n", line);
                 }
-            } else {}
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -47,7 +88,7 @@ public class CommandCLI implements Callable<Integer> {
 
     @Override
     public Integer call() throws Exception {
-        if (isPipe) {
+        if (isPipe || file != null) {
             readFromPipe();
             return 0;
         }
